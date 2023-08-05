b3e3c970c70b2bd7420129691cd145449bf53c7b
sahancodes <141490700+sahancodes@users.noreply.github.com>
2023-08-05 23:01:31 +0200

    create documents collections - firebase

--- frontend/lib/controllers/questionpaper/data_uploader.dart
+++ frontend/lib/controllers/questionpaper/data_uploader.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:frontend/firebase_ref/references.dart';
 import 'package:frontend/models/question_paper_model.dart';
 import 'package:get/get.dart';
 
@@ -14,6 +16,8 @@ class DataUploader extends GetxController {
   }
 
   Future<void> uploadData() async {
+    final fireStore = FirebaseFirestore.instance;
+
     final manifestContent = await DefaultAssetBundle.of(Get.context!)
         .loadString("AssetManifest.json"); //! means not null
     final Map<String, dynamic> manifestMap = jsonDecode(manifestContent);
@@ -32,6 +36,39 @@ class DataUploader extends GetxController {
       questionPapers
           .add(QuestionPaperModel.fromJson(jsonDecode(stringPaperContent)));
     }
-    print('Items number ${questionPapers[0].description}');
+    //print('Items number ${questionPapers[0].description}');
+
+    var batch = fireStore.batch();
+
+    for (var paper in questionPapers) {
+      batch.set(questionPaperRF.doc(paper.id), {
+        "title": paper.title,
+        "image_url": paper.imageUrl,
+        "Description": paper.description,
+        "time_seconds": paper.timeSeconds,
+        "questions_count": paper.questions == null ? 0 : paper.questions!.length
+      });
+
+      for (var questions in paper.questions!) {
+        final questionPath =
+            questionRF(paperId: paper.id, questionId: questions.id);
+        batch.set(questionPath, {
+          "question": questions.question,
+          "correct_answer": questions.correctAnswer,
+        });
+
+        for (var answer in questions.answers) {
+          batch.set(questionPath.collection("answers").doc(answer.identifier), {
+            "identifier": answer.identifier,
+            "answer": answer.answer,
+          });
+        }
+      }
+    }
+
+    //1.31.43 youtube
+
+    await batch.commit();
+    print("Batch committed!");
   }
 }
